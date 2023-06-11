2df5c1a49b577e674b1494daf4a81fa9399a0d2d
Dayana Priscilla <daypnfsi@gmail.com>
2023-06-11 00:35:05 -0300

    limpando code

--- App_cursoK_liscacurso/ListaVipApp/app/src/main/java/com/example/listavipapp/view/MainActivity.java
+++ App_cursoK_liscacurso/ListaVipApp/app/src/main/java/com/example/listavipapp/view/MainActivity.java
@@ -1,20 +1,14 @@
 package com.example.listavipapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.viewmodel.CreationExtras;
-
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.example.listavipapp.R;
 import com.example.listavipapp.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
-
 
     Pessoa pessoa;
     EditText editTextPrimeiroNome;
@@ -24,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     Button btn_limpar;
     Button btn_salvar;
     Button btn_finalizar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,55 +28,42 @@ public class MainActivity extends AppCompatActivity {
 
         pessoa = new Pessoa();
 
-
         //jogando os dados para os campos edit's
         setEdit();
 
         //limpando os dados nos campos edit's
-        btn_limpar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                btn_limpar_campos();
-            }
-        });
-        btn_finalizar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                btn_finalizar_activity();
-            }
-        });
-        btn_salvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btn_limpar.setOnClickListener(v -> btn_limpar_campos());
 
-                if(editTextPrimeiroNome.getText().toString().isEmpty() ||
-                        editTextSobrenome.getText().toString().isEmpty() ||
-                        editTextCursoDesejado.getText().toString().isEmpty() ||
-                        editTextTelefone.getText().toString().isEmpty()){
-                    Toast.makeText(MainActivity.this,"Preencha todos os campos",Toast.LENGTH_LONG).show();
-                }else{
+        btn_finalizar.setOnClickListener(v -> btn_finalizar_activity());
 
-                    pessoa.setPrimeironome(editTextPrimeiroNome.getText().toString());
-                    pessoa.setSobrenome(editTextSobrenome.getText().toString());
-                    pessoa.setCursoDesejado(editTextCursoDesejado.getText().toString());
-                    pessoa.setTelefoneContato(editTextTelefone.getText().toString());
-                    Toast.makeText(MainActivity.this,"Salvo",Toast.LENGTH_LONG).show();
-                }
+        btn_salvar.setOnClickListener(v -> {
+
+            if (editTextPrimeiroNome.getText().toString().isEmpty() ||
+                    editTextSobrenome.getText().toString().isEmpty() ||
+                    editTextCursoDesejado.getText().toString().isEmpty() ||
+                    editTextTelefone.getText().toString().isEmpty()) {
+                Toast.makeText(MainActivity.this, "Preencha todos os campos", Toast.LENGTH_LONG).show();
+            } else {
+
+                pessoa.setPrimeironome(editTextPrimeiroNome.getText().toString());
+                pessoa.setSobrenome(editTextSobrenome.getText().toString());
+                pessoa.setCursoDesejado(editTextCursoDesejado.getText().toString());
+                pessoa.setTelefoneContato(editTextTelefone.getText().toString());
+                Toast.makeText(MainActivity.this, "Salvo", Toast.LENGTH_LONG).show();
             }
         });
 
-
-       // Log.i("DADOS PESSOA: ",pessoa.toString());
+        // Log.i("DADOS PESSOA: ",pessoa.toString());
     }
-     public void inicializacao(){
-         editTextPrimeiroNome = findViewById(R.id.edit_primeironome);
-         editTextSobrenome = findViewById(R.id.edit_sobrenome);
-         editTextCursoDesejado = findViewById(R.id.edit_nomedocurso);
-         editTextTelefone= findViewById(R.id.edit_telefone);
+    public void inicializacao() {
+        editTextPrimeiroNome = findViewById(R.id.edit_primeironome);
+        editTextSobrenome = findViewById(R.id.edit_sobrenome);
+        editTextCursoDesejado = findViewById(R.id.edit_nomedocurso);
+        editTextTelefone = findViewById(R.id.edit_telefone);
 
-     }
+    }
 
-     public void setEdit(){
+    public void setEdit() {
         editTextPrimeiroNome.setText(pessoa.getPrimeironome());
         editTextSobrenome.setText(pessoa.getSobrenome());
         editTextCursoDesejado.setText(pessoa.getCursoDesejado());
@@ -89,18 +71,17 @@ public class MainActivity extends AppCompatActivity {
         btn_limpar = findViewById(R.id.btn_limpar);
         btn_salvar = findViewById(R.id.btn_salvar);
         btn_finalizar = findViewById(R.id.btn_finalizar);
-
     }
+
     //metodo para limpar os campos
-    public void btn_limpar_campos(){
+    public void btn_limpar_campos() {
         editTextPrimeiroNome.setText("");
         editTextSobrenome.setText("");
         editTextCursoDesejado.setText("");
         editTextTelefone.setText("");
-
     }
-    public void btn_finalizar_activity(){
+
+    public void btn_finalizar_activity() {
         finish();
     }
-
 }
