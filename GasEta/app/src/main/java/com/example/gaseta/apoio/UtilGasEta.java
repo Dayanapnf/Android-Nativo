54440450ef9e9168272237b94a073a4695f130d3
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-01-21 16:55:52 -0300

    up

---
+++ GasEta/app/src/main/java/com/example/gaseta/apoio/UtilGasEta.java
@@ -0,0 +1,5 @@
+package com.example.gaseta.apoio;
+
+public class UtilGasEta {
+    
+}
--- GasEta/app/src/main/java/com/example/gaseta/view/MainActivity.java
+++ GasEta/app/src/main/java/com/example/gaseta/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.gaseta;
+package com.example.gaseta.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.gaseta.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        finishActivity();
     }
 }
