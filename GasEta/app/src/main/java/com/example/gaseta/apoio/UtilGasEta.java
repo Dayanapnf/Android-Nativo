de04cb77e8dd34a2611aa4c2ad88f17c76da3e8f
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-01-21 22:07:56 -0300

    up

--- GasEta/app/src/main/java/com/example/gaseta/apoio/UtilGasEta.java
+++ GasEta/app/src/main/java/com/example/gaseta/apoio/UtilGasEta.java
@@ -1,5 +1,25 @@
 package com.example.gaseta.apoio;
 
 public class UtilGasEta {
-    
+   public static String calcularMelhorOpcao(Double precoGasolina, Double precoEtanol){
+       String resultado = "";
+
+        // Verifica se ambos os preços são não negativos
+       if (precoGasolina >= 0 && precoEtanol >= 0) {
+           // Calcula a relação entre o preço do etanol e da gasolina
+           double relacaoPreco = precoEtanol / precoGasolina;
+
+           // Compara a relação de preços e determina a melhor opção
+           if (relacaoPreco < 0.7) {
+               resultado = "Melhor opção: Etanol";
+           } else {
+               resultado = "Melhor opção: Gasolina";
+           }
+       } else {
+           resultado = "Os preços não podem ser negativos";
+       }
+
+
+       return resultado;
+   }
 }
--- GasEta/app/src/main/java/com/example/gaseta/view/MainActivity.java
+++ GasEta/app/src/main/java/com/example/gaseta/view/MainActivity.java
@@ -3,14 +3,100 @@ package com.example.gaseta.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.gaseta.R;
+import com.example.gaseta.apoio.UtilGasEta;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonCalcular;
+    ImageButton imageButtonLimpar;
+    FloatingActionButton floatingActionButtonFinalizar;
+    FloatingActionButton floatingActionButtonSalvar;
+    TextView textViewResultado;
+    EditText editTextGasolina;
+    EditText editTextEtanol;
+    double precoGasolina, precoEtanol;
+    String result;
+    boolean isDadosOk = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inicializarComponentes();
+
+        buttonCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                functionCalcular();
+
+            }
+        });
+        imageButtonLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textViewResultado.setText("");
+                editTextEtanol.setText("");
+                editTextGasolina.setText("");
+
+
+            }
+        });
+        floatingActionButtonFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Volte sempre!", Toast.LENGTH_LONG ).show();
+                finish();
+            }
+        });
+        floatingActionButtonSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    public void inicializarComponentes(){
+        editTextEtanol = findViewById(R.id.editTextEtanol);
+        editTextGasolina = findViewById(R.id.editTextGasolina);
+        textViewResultado = findViewById(R.id.textViewResultado);
+        floatingActionButtonFinalizar = findViewById(R.id.floatingActionButtonFinalizar);
+        floatingActionButtonSalvar = findViewById(R.id.floatingActionButtonSalvar);
+        imageButtonLimpar = findViewById(R.id.imageButtonLimpar);
+        buttonCalcular = findViewById(R.id.buttonCalcular);
+    }
+    public void functionCalcular(){
+
+        if(TextUtils.isEmpty(editTextGasolina.getText())){
+            editTextGasolina.setError("Obrigatório");
+            editTextGasolina.requestFocus();
+            isDadosOk = false;
+        }if (TextUtils.isEmpty(editTextEtanol.getText())){
+            editTextEtanol.setError("Obrigatório");
+            editTextEtanol.requestFocus();
+            isDadosOk = false;
+        }if(isDadosOk){
+           precoGasolina = Double.parseDouble(editTextGasolina.getText().toString());
+           precoEtanol = Double.parseDouble(editTextEtanol.getText().toString());
+           result = UtilGasEta.calcularMelhorOpcao(precoGasolina,precoEtanol);
+           textViewResultado.setText(result);
+        }else{
+            Toast.makeText(MainActivity.this, "Preencha todos os campos!", Toast.LENGTH_LONG ).show();
+
+        }
+
+
     }
 }
